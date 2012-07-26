3c1edf8132a05a2d321b686b1901df632976920b
Joakim Nyman <joakim@edgefive.com>
2012-07-26 19:13:43 +0300

    Added Swedish translation of Formz

---
+++ core/components/formz/lexicon/sv/properties.inc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * formz
+ *
+ * @package formz
+ */
+/**
+ * Properties Swedish Lexicon Entries for formz
+ *
+ * @package formz
+ * @subpackage lexicon
+ */
+$_lang['prop_formz.tpl'] = 'Standard Formz mall';
+$_lang['prop_formz.id'] = 'Formulär ID';
+$_lang['prop_formz.field_tpl'] = 'Standard Formz fält mall';
